dff6d468ef560b1135e9cefbb7840a822f0a3645
kirill <kirill_nn@inbox.ru>
2018-01-14 17:27:06 +0300

    Part 3

--- app/src/main/java/ru/pks/filmlibrary/Genre.java
+++ app/src/main/java/ru/pks/filmlibrary/Genre.java
@@ -3,24 +3,26 @@ package ru.pks.filmlibrary;
 public class Genre {
 
     static final Genre[] genres = {
-            new Genre("Боевик"),
-            new Genre("Вестерн"),
-            new Genre("Детектив"),
-            new Genre("Детское кино"),
-            new Genre("Документальное кино"),
-            new Genre("Драма"),
-            new Genre("Комедия"),
-            new Genre("Приключение"),
-            new Genre("Семейное кино"),
-            new Genre("Триллер"),
-            new Genre("Ужасы"),
-            new Genre("Фантастика")
+            new Genre("Боевик", ""),
+            new Genre("Вестерн", ""),
+            new Genre("Детектив", ""),
+            new Genre("Детское кино", ""),
+            new Genre("Документальное кино", ""),
+            new Genre("Драма", ""),
+            new Genre("Комедия",""),
+            new Genre("Приключение",""),
+            new Genre("Семейное кино",""),
+            new Genre("Триллер",""),
+            new Genre("Ужасы",""),
+            new Genre("Фантастика","")
     };
 
     private String name;
+    private String description;
 
-    private Genre(String name) {
+    private Genre(String name, String description) {
         this.name = name;
+        this.description = description;
     }
 
     public String getName() {
