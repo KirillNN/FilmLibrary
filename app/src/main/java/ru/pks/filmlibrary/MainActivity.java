f4a1b841c62df17b9f8520a2d08de09fb74d0c48
kirill <kirill_nn@inbox.ru>
2018-01-14 17:20:58 +0300

    Part 2

--- app/src/main/java/ru/pks/filmlibrary/MainActivity.java
+++ app/src/main/java/ru/pks/filmlibrary/MainActivity.java
@@ -1,9 +1,11 @@
 package ru.pks.filmlibrary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         genres = Arrays.asList(Genre.genres);
 
+    }
+
+    private class GenreAdapter extends RecyclerView.Adapter<GenreViewHolder> {
+        Context context;
+
+        public GenreAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public GenreViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(GenreViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 
     private class GenreViewHolder extends RecyclerView.ViewHolder {
