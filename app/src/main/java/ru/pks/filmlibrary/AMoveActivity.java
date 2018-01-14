d19ddc1fd195cff32c3be613321b1e48fab11f12
kirill <kirill_nn@inbox.ru>
2018-01-14 21:49:56 +0300

    Part 11

--- app/src/main/java/ru/pks/filmlibrary/AMoveActivity.java
+++ app/src/main/java/ru/pks/filmlibrary/AMoveActivity.java
@@ -2,17 +2,34 @@ package ru.pks.filmlibrary;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AMoveActivity extends AppCompatActivity {
+    private RecyclerView amove;
+    private List<ActionMovie> amoveList;
+    private AMoveAdapter aMoveAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amove);
+
+        amoveList = Arrays.asList(ActionMovie.amove);
+        aMoveAdapter = new AMoveAdapter(this, amoveList);
+        amove = findViewById(R.id.amove_list_recycler_view);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+
+        amove.setLayoutManager(gridLayoutManager);
+        amove.setAdapter(aMoveAdapter);
+
     }
 }
 class AMoveAdapter extends RecyclerView.Adapter<AMoveViewHolder> {
